844468d83cd79e44a733ef79d6340d15b21555f7
tommorow4123 <46416761+tommorow4123@users.noreply.github.com>
2019-04-10 10:29:26 +0900

    portfolio 変更

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
 $(function(){
      $('#button') .click(function(){
-          //    alert("aaa");
-          // $('#nav-content').addClass('hidden');
-          //$('#nav-content').removeClass('hidden');
-          
         $('#list').slideToggle(200);
      });
-     //$('#button').click(function(){
-      // $('#list').fadeToggle(200);
-     //});
+
+
+     
 });
