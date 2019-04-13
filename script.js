e93175882240d8aa4fbacab43afc380133298ac1
tommorow4123 <46416761+tommorow4123@users.noreply.github.com>
2019-04-13 19:23:17 +0900

    portfolio

--- script.js
+++ script.js
@@ -2,7 +2,14 @@ $(function(){
      $('#button') .click(function(){
         $('#list').slideToggle(200);
      });
-
-
-     
+     $(function(){
+      $('a[href^="#"]').click(function(){
+        var speed = 500;
+        var href= $(this).attr("href");
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        var position = target.offset().top;
+        $("html, body").animate({scrollTop:position}, speed, "swing");
+        return false;
+      });
+    });
 });
